ddbe0d04b92ed7738b344063f8be6a30bc52dfb2
Ryan Carlo Negretti Pereira <160555754+RyanCarloNegrettiPereira@users.noreply.github.com>
2024-05-28 09:31:57 -0300

    Delete Aula06 directory

--- Aula06/exercicio01.py
+++
@@ -1,6 +0,0 @@
-print(f"Identificador de diviores (5 e 3).")
-a=int(input(f"Digite um Número: "))
-if (a%5==0) and (a%3==0):
-    print(f"{a} é divisível por 5 e 3.")
-else:
-    print(f"{a} não é divisível por 5 e 3.")
