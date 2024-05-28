c4588f78bb29e6ba6397ed15332eb26ccc3bc5c1
Ryan Carlo Negretti Pereira <160555754+RyanCarloNegrettiPereira@users.noreply.github.com>
2024-05-28 09:32:13 -0300

    Delete Aula07 directory

--- Aula07/ex0.1.py
+++
@@ -1,7 +0,0 @@
-valor=int(input(f'Digite um valor inteiro e positivo para "n": '))
-n=1
-soma=0
-while n<=valor:
-    soma=soma+(2**n)
-    n=n+1
-print(f'O valor de E=((2¹)+...+(2^{valor}) é: {soma}')
--- Aula07/ex1.0.py
+++
@@ -1,7 +0,0 @@
-n=0
-soma=0
-for i in range(0,100):
-    n=n+1
-    if (n%2)==0:
-        soma=soma+n
-print(f'A soma dos primeiros 50 números pares é: {soma}')
