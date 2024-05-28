0c8f147889a749343d492ad2b75eadf06565e746
Ryan Carlo Negretti Pereira <160555754+RyanCarloNegrettiPereira@users.noreply.github.com>
2024-05-28 09:32:38 -0300

    Delete Aula14 directory

--- Aula14/ex00.py
+++
@@ -1,8 +0,0 @@
-y=int(input(f'Digite o total de cabeças: '))
-pes=y*3+3
-z=pes/2-y
-x=y-z
-print(f'Total de cabeças: {y}\n'
-      f'Total de pés: {pes}\n'
-      f'Total de coelhos: {z}\n'
-      f'total de patos: {x}')
--- Aula14/ex02.py
+++
@@ -1,8 +0,0 @@
-ra=input('Digite seu RA: ')
-soma=0
-multiplicador=1
-for i in range(len(ra)):
-    soma+=int(ra[i])
-    multiplicador*=int(ra[i])
-print(f'A soma é: {soma}\n'
-      f'A multiplicação é: {multiplicador}')
