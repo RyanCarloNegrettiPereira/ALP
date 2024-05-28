d39cf1e944a3227528ed3b48fc6af13374cd0ddd
Ryan Carlo Negretti Pereira <160555754+RyanCarloNegrettiPereira@users.noreply.github.com>
2024-05-28 09:32:29 -0300

    Delete Aula09 directory

--- Aula09/ex01.py
+++
@@ -1,6 +0,0 @@
-lista=[]
-for i in range(5):
-    v=int(input(f"Digite o {i}º valor: "))
-    lista.append(v)
-print(f"O maior item da lista é: {max(lista)}")
-print(f"Sua posição é: {lista.index(max(lista))}")
