a116a35e7e0c852a1598ea319d3d4ee3a7da9c01
Ryan Carlo Negretti Pereira <160555754+RyanCarloNegrettiPereira@users.noreply.github.com>
2024-05-28 09:32:21 -0300

    Delete Aula08 directory

--- Aula08/ex02.py
+++
@@ -1,9 +0,0 @@
-frase=input(f'Digite uma frase: ').lower()
-v=0
-for letra in frase:
-    if letra in "aáàâãeéêiíoóôõuúû":
-        v=v+1
-if v==1:
-    print(f'Em sua frase temos {v} vogal.')
-else:
-    print(f'Em sua frase temos {v} vogais.')
--- Aula08/ex04.py
+++
@@ -1,6 +0,0 @@
-palavra=input(f'Digite uma palavra: ')
-arvalap=palavra[::-1]
-if palavra==arvalap:
-    print(f'Sua palavra é um palíndromo.')
-else:
-    print(f'Sua palavra não é um palíndromo.')
